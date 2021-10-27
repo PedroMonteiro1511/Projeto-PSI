a65c75c35c66b42650b08ac36d43266ad9c82d16
jose <jose.asantosramos@gamil.com>
2021-10-27 13:53:09 +0100

    primeiro commit

---
+++ projeto/frontend/views/site/index.php
@@ -0,0 +1,41 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-4">Bem-vindo!</h1>
+
+        <p class="lead">Começa agora a vendar e comprar o que precisas.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://projeto-frontend.test/index.php?r=site%2Flogin">Login!</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+
+            <div class="col-lg-4">
+                <h2>Artigos</h2>
+
+                <p>Começa já a vender, comprar e trocar os teus produtos favoritos.</p>
+
+                <p><a class="btn btn-outline-secondary" href="http://projeto-frontend.test/index.php?r=produto%2Findex">Ver artigos &raquo;</a></p>
+            </div>
+            <div class="col-lg-2">
+
+            </div>
+            <div class="col-lg-4">
+                <h2>Leilões</h2>
+
+                <p>Leiloar ...</p>
+
+                <p><a class="btn btn-outline-secondary" href="http://projeto-frontend.test/index.php?r=leilao%2Findex">Ver Leilões&raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
