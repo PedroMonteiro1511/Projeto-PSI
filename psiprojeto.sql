bcc787691ffef4b01e2484878325dae0bcb3c282
PedroMonteiro1511 <2190769@my.ipleiria.pt>
2021-11-17 11:32:40 +0000

    Versão atualizada da base de dados
    Tabelas : Leilão , Venda.

--- psiprojeto.sql
+++ psiprojeto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 09-Nov-2021 às 00:10
+-- Tempo de geração: 17-Nov-2021 às 11:31
 -- Versão do servidor: 5.7.31
 -- versão do PHP: 7.3.21
 
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `auth_assignment` (
 
 INSERT INTO `auth_assignment` (`item_name`, `user_id`, `created_at`) VALUES
 ('gestor', '2', NULL),
-('login-admin', '1', NULL);
+('login-admin', '24', NULL);
 
 -- --------------------------------------------------------
 
@@ -111,6 +111,25 @@ CREATE TABLE IF NOT EXISTS `auth_rule` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `leilao`
+--
+
+DROP TABLE IF EXISTS `leilao`;
+CREATE TABLE IF NOT EXISTS `leilao` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idUser` int(11) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `descricao` text NOT NULL,
+  `datalimite` datetime NOT NULL,
+  `precobase` decimal(10,0) NOT NULL,
+  `aprovado` enum('S','N') NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`id`),
+  KEY `idUser` (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `migration`
 --
 
@@ -149,24 +168,39 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `status` smallint(6) NOT NULL DEFAULT '10',
-  `created_at` int(11) DEFAULT NULL,
-  `updated_at` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `verification_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `password_reset_token` (`password_reset_token`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`, `verification_token`) VALUES
-(1, 'Monteiro', 'HjaVmMqGqGrs6BLDBA0ZyNCZiRIbDm1B', '$2y$13$SHuZXF6t1CBH.IR61llHN.HamUYA1GNtlbw.typsRB4b9ChsRWVLO', '', 'ppedromonteiroescola@outlook.pt', 10, 1636206612, 1636206612, '-4OieUlH0T_efyvaXpl06AV-1vQAYu2C_1636206612'),
-(2, 'MonteiroGestor', 'lroZV_MJYB0AtmSvcYbXCrbaWHjUj9nP', '$2y$13$0vHef9B7GA1qIGk4OVQ2b.qYsum5uUKXvwYbZ3RDuaajCUlcAd2Qi', NULL, 'pedromonteiroescola2@outlook.pt', 10, 1636208093, 1636208093, 'SNrjmMt0zpv5cRFOzaTAe2h1WYcQd80g_1636208093'),
-(3, 'MonteiroGuest', 'wg5vuJF_ShC8QuplO9I-VxFT7WzjoDIF', '$2y$13$/kRV.csFfYlyWpYdM8/9AuMeU.8a9Hu34npxfTSiIxjSSRUdIujL6', NULL, 'MonteiroGuest@hotmail.com', 10, 1636214125, 1636214125, 'hb9mx8QEYIGop6DrorHc8hscRvcok9RN_1636214125'),
-(22, 'joseramos', 'sxLSdNpObujxwlGNlUQp42sJ7F3WChFV', '$2y$13$tVw5qnVfI6xZVOQLMp4tVeBP64fMgDyI37FqcqMbwUUMMNTKYuKh2', NULL, 'josesantos@gmail.com', 10, 1636413964, 1636413964, 'LGBNQtfVHEPREKbh81EFTpnlX6Mtos5l_1636413964');
+(24, 'MonteiroAdmin', 'PRDzd184_UxQCHHpXP2okv_0Ver6hXIp', '$2y$13$EV49o3Lc8JKQE3YSx4Fwy.d01dl9tO5o/.Dk8ATXvGulWv.3Pwteq', NULL, 'Monteiro@gmail.com', 10, '2021-11-17 10:38:16', '2021-11-17 10:38:16', NULL),
+(26, 'Monteiro2', 'l8ngJAer_fRe89itTBY_kuZ71rXfZ7SS', '$2y$13$VsIBZh9mIifQ/gy7cYijJeoTal72W4X43FVST4BgXOikMOrB1cPle', NULL, 'Monteiro2@gmail.com', 10, '2021-11-17 10:57:57', '2021-11-17 10:57:57', '0aKabNfWEVvL7WOv8qmIUhAnAgDY5Skc_1637146677');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda`
+--
+
+DROP TABLE IF EXISTS `venda`;
+CREATE TABLE IF NOT EXISTS `venda` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idUser` int(11) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `descricao` text NOT NULL,
+  `preco` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `userVenda` (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Restrições para despejos de tabelas
@@ -190,6 +224,18 @@ ALTER TABLE `auth_item`
 ALTER TABLE `auth_item_child`
   ADD CONSTRAINT `auth_item_child_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `auth_item_child_ibfk_2` FOREIGN KEY (`child`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `leilao`
+--
+ALTER TABLE `leilao`
+  ADD CONSTRAINT `userLeilao` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`);
+
+--
+-- Limitadores para a tabela `venda`
+--
+ALTER TABLE `venda`
+  ADD CONSTRAINT `userVenda` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
